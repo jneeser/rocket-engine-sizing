e20a51160bc84b29072ec98d2a45b63373562962
jneeser <jones.neeser99@gmail.com>
2023-11-24 14:52:36 +0100

    update to propellants

--- PreliminarySizing/PropSweeps.py
+++ PreliminarySizing/PropSweeps.py
@@ -32,16 +32,51 @@ def isp_plots(fuels, oxidisers, Pc, eps, MR_range, P_amb):
 	plt.xlabel('MR')
 	plt.ylabel('Isp [s]')
 	plt.legend(loc='best')
-	plt.title('Pc = ' + str(round(Pc/1e5,2)) + ' bar,' + ' eps = ' + str(eps) + ', P_amb = ' + str(round(P_amb/1e5,2)))
+	plt.title('Pc = ' + str(round(Pc/1e5,2)) + ' bar,' + ' eps = ' + str(eps) + ', P_amb = ' + str(round(P_amb/1e5,2)) + ' bar')
+	plt.savefig('Isp.png', dpi=600)
+	plt.show()
+	
+
+
+def temperature_plots(fuels, oxidisers, Pc, eps, MR_range, P_amb):
+
+	T_arr = np.ndarray((len(fuels), len(MR_range)))
+	
+	# matplotlib linestyles
+	lines = ["-","--","-.",":","-"]
+	linecycler = cycle(lines)
+	
+	for i in range(len(fuels)):
+		cea = BipropCEA(fuels[i], oxidisers[i], Pc, P_amb)
+		
+		for j in range(len(MR_range)):
+			cea.metric_cea_output('chamber', MR_range[j], eps)
+			# calculate vacuum isp
+			T_arr[i][j] = cea.Tc
+
+	# plot isp data
+	for i in range(len(fuels)):
+		plt.plot(MR_range, T_arr[i], next(linecycler), color='r')
+	
+	plt.vlines(42.17, 1000, 3200, linestyles='--', color='k', label='lean extinction limit')
+	plt.vlines(1.27, 1000, 3200, linestyles=':', color='k', label='rich extinction limit')
+	plt.vlines(3.945, 1000, 3200, linestyles='-.', color='b', label='stoichiometric')
+	plt.xlabel('O/F')
+	plt.ylabel('Adiabatic Flame Temperature (K)')
+	plt.legend(loc='best')
+	#plt.title('Pc = ' + str(round(Pc/1e5,2)) + ' bar,' + ' eps = ' + str(eps) + ', P_amb = ' + str(round(P_amb/1e5,2)) + ' bar')
+	plt.savefig('Temperature.png', dpi=600)
 	plt.show()
 
 
+
 if __name__ == "__main__":
-	fuels     = ['MMH', pl.TMPDA, 'C2H6', 'C2H5OH']
-	oxidisers = ['N2O4', pl.peroxide98, 'N2O', pl.peroxide95]
+	fuels     = ['MMH', pl.TMPDA, 'Propylene', 'Propylene', 'C2H6']
+	oxidisers = ['N2O4', pl.peroxide98, 'N2O', pl.peroxide98, pl.peroxide98]
 	Pc        = 10e5                    # chamber pressure [Pa]
 	P_amb     = 10
 	eps       = 220                         # expansion ratio
-	MR_range  = np.linspace(1.5, 14, 99)  # mixture ratio range
+	MR_range  = np.linspace(1.5, 12, 99)  # mixture ratio range
 
 	isp_plots(fuels, oxidisers, Pc, eps, MR_range, P_amb)
+	temperature_plots(['CH4'], ['GOX'], 3e5, 1, np.linspace(1,45,500), 1e5)
